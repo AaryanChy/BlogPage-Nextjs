28a3e47d9de68ca2940c3fa8493fa7033befcf1e
Aaryan Chaudhary <aaryanchaudhary188@gmail.com>
2024-05-19 14:20:14 +1000

    designed navbar and links

--- src/components/navbar/links/Links.js
+++ src/components/navbar/links/Links.js
@@ -1,10 +1,13 @@
+"use client";
 import Link from "next/link";
 import styles from "./links.module.css";
 import NavLink from "./navLink/navLink";
+
 const Links = () => {
+  const [open, setOpen] = useState(false);
   const links = [
     {
-      title: "home",
+      title: "Home",
       path: "/",
     },
     {
@@ -16,15 +19,25 @@ const Links = () => {
       path: "/contact",
     },
     {
-      title: "blog",
+      title: "Blog",
       path: "/blog",
     },
   ];
+  const session = true;
+  const isAdmin = false;
   return (
     <div className={styles.link}>
       {links.map((link) => (
         <NavLink item={link} key={link.title} />
       ))}
+      {session ? (
+        <>
+          {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
+          <button className={styles.logout}>Logout</button>
+        </>
+      ) : (
+        <NavLink item={{ title: "Login", path: "/login" }} />
+      )}
     </div>
   );
 };
